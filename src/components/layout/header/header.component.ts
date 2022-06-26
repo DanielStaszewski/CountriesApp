c4c4dd05676ef65d15a3ee1269bec61d6ce3fb3b
DanielS <gorn225@interia.pl>
2022-06-26 11:49:36 +0200

    implement go back function

--- src/components/layout/header/header.component.ts
+++ src/components/layout/header/header.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'header-component',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private rootRoute = '/regions';
+  private subscription = new Subscription();
+  public showGoBackButton = false;
+
+  constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+
+    this.subscription.add(
+    this.router.events.pipe(filter(event => event instanceof NavigationEnd))
+    .subscribe((event: NavigationEnd) => 
+     {
+        if(event.url === this.rootRoute) this.showGoBackButton = false;
+        else this.showGoBackButton = true;          
+     })
+    )
+
+  }
+
+  goBack(): void {
+    this.router.navigate([this.router.url.substring(0, this.router.url.lastIndexOf('/'))]);
+  }
+
+  ngOnDestroy(): void{
+    this.subscription.unsubscribe();
   }
 
 }
